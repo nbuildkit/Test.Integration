29f857df70d8ccd9f8b7a981e50daa64fe9ea8e6
Petrik van der Velde <petrikvandervelde@gmail.com>
2017-07-02 15:56:34 +1200

    Fixing compiler errors

--- 0.9/vs-solution-01/src/Test.Unit.VbNet.Library/My Project/Settings.Designer.vb
+++ 0.9/vs-solution-01/src/Test.Unit.VbNet.Library/My Project/Settings.Designer.vb
@@ -57,16 +57,16 @@ Namespace My
 End Namespace
 
 Namespace My
-    
+
     <Global.Microsoft.VisualBasic.HideModuleNameAttribute(),  _
      Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
      Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()>  _
     Friend Module MySettingsProperty
-        
+
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.Test.Unit.Library.My.MySettings
+        Friend ReadOnly Property Settings() As Global.Test.Unit.VbNet.Library.My.MySettings
             Get
-                Return Global.Test.Unit.Library.My.MySettings.Default
+                Return Global.Test.Unit.VbNet.Library.My.MySettings.Default
             End Get
         End Property
     End Module
